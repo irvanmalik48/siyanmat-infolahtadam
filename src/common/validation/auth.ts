1f0cc39f82e6013af986d98d86935991191a3ec0
Irvan Malik <irvanmalik48@gmail.com>
2023-05-23 12:35:09 +0700

    feat(common): trpc and next-auth integration
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -2,9 +2,8 @@ import * as z from "zod";
 
 export const loginSchema = z.object({
   email: z.string().email(),
-  name: z.string(),
+  name: z.string().optional(),
   password: z.string(),
 });
 
 export type ILogin = z.infer<typeof loginSchema>;
-export type ISignUp = z.infer<typeof loginSchema>;
