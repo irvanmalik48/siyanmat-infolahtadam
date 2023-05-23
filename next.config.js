4b8d835e09c7d7d06639772240b5818b59635a43
Irvan Malik <irvanmalik48@gmail.com>
2023-05-23 12:33:57 +0700

    fix(next): remove million for breaking the app
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-import million from 'million/compiler';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-export default million.next(nextConfig);
+module.exports = nextConfig;
