f16daba7d2d1e05dc75c2171ac9085d6c1bbca16
Irvan Malik <irvanmalik48@gmail.com>
2023-05-24 21:04:56 +0700

    feat(kegiatan/tambah): delete unused deps
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/kegiatan/tambah/index.tsx
+++ src/pages/kegiatan/tambah/index.tsx
@@ -4,7 +4,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { trpc } from "@/common/trpc";
 import { useCallback, useEffect, useState } from "react";
-import ReactDatePicker from "react-datepicker";
 import { useRouter } from "next/router";
 import { PeralatanOptions } from "@/components/Options";
 
