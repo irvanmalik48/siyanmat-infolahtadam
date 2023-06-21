9daed6310196f350c02aa0794eb3ea70fc68520a
Irvan Malik <irvanmalik48@gmail.com>
2023-06-21 17:22:18 +0700

    feat(comp/navigationcard): add navigation card
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/components/functional/NavigationCard.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+interface Props {
+  href?: string;
+  icon: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export default function NavigationCard({ href, icon, children }: Props) {
+  return (
+    <Link
+      href={href ?? "#"}
+      className="flex flex-col w-full gap-2 p-5 transition border rounded-xl border-neutral-300 hover:bg-celtic-50 hover:bg-opacity-50 group hover:border-celtic-800 hover:ring-4 hover:ring-celtic-800 hover:ring-opacity-50"
+    >
+      <div className="p-5 text-black transition rounded-full w-fit bg-neutral-200 group-hover:bg-celtic-800 group-hover:text-white">
+        {icon}
+      </div>
+      <span className="text-lg font-semibold">{children}</span>
+    </Link>
+  )
+}
