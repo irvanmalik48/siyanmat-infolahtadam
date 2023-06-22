20b541e97454e5d19b4b5c9e971bfb907a370a44
Irvan Malik <irvanmalik48@gmail.com>
2023-06-22 17:09:48 +0700

    feat(app): i don't know
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,8 +1,15 @@
+import QuickNav from "@/components/aesthetic/QuickNav";
+
 export const metadata = {
   title: "Dashboard",
   description: "Dashboard SIYANMAT",
 };
 
 export default function Home() {
-  return <></>;
+  return (
+    <section className="w-full max-w-4xl px-5 mx-auto">
+      <h1 className="w-full text-4xl font-bold leading-snug mt-14">Apa yang ingin anda<br />lakukan hari ini?</h1>
+      <QuickNav />
+    </section>
+  );
 }
