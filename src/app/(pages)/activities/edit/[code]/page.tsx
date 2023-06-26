4e396c9ffd8d5526bfaa79ddaa8ec9e444b3b7a7
Irvan Malik <irvanmalik48@gmail.com>
2023-06-26 15:55:35 +0700

    feat(app/activities/edit): route done
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/app/(pages)/activities/edit/[code]/page.tsx
@@ -0,0 +1,39 @@
+import { Metadata, ResolvingMetadata } from "next";
+import EditActivitySection from "@/components/functional/EditActivitySection";
+
+export async function generateMetadata({
+  params: {
+    code,
+  },
+  parent
+}: {
+  params: {
+    code: string;
+  },
+  parent: ResolvingMetadata
+}): Promise<Metadata> {
+  const toolCode = code;
+
+  return {
+    title: `Edit Kegiatan - ${toolCode}`,
+    description: `Edit Kegiatan - ${toolCode}`,
+  };
+}
+
+export default function AddTool({
+  params: {
+    code,
+  }
+}: {
+  params: {
+    code: string;
+  }
+}) {
+
+  return (
+    <section className="w-full max-w-4xl px-5 mx-auto">
+      <h1 className="w-full text-4xl font-bold leading-snug mt-14">Edit Kegiatan</h1>
+      <EditActivitySection code={code} />
+    </section>
+  );
+}
